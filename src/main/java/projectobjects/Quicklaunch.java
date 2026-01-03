9099875368d290874e968c32efa827aed15007de
Mehak Chopra <cmehak26@gmail.com>
2026-01-03 15:15:12 +0530

    add new file

--- src/main/java/projectobjects/Quicklaunch.java
+++ src/main/java/projectobjects/Quicklaunch.java
@@ -1,6 +1,16 @@
 package projectobjects;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Quicklaunch extends Dashboard {
+	WebDriverWait wait;
 	
+	public QuickLaunch(WebDriver driver) {
+		super(driver);
+		wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+	}
 
 }
